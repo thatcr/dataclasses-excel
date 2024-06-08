edc3d84605590267363382bce0239ca60c3d44e0
Rob Thatcher <rob@tin.works>
2024-06-08 06:48:21 +0100

    add ruff pre-commit

--- src/dataclasses_excel/cli/__init__.py
+++ src/dataclasses_excel/cli/__init__.py
@@ -6,7 +6,10 @@ import click
 from dataclasses_excel.__about__ import __version__
 
 
-@click.group(context_settings={"help_option_names": ["-h", "--help"]}, invoke_without_command=True)
+@click.group(
+    context_settings={"help_option_names": ["-h", "--help"]},
+    invoke_without_command=True,
+)
 @click.version_option(version=__version__, prog_name="dataclasses-excel")
 def dataclasses_excel():
     click.echo("Hello world!")
