0e5435f0a5b31951e6964e89fd31ed33f530d90a
Rob Thatcher <rob@tin.works>
2024-06-13 21:23:28 +0100

    notes on resolving

--- src/dataclasses_excel/__init__.py
+++ src/dataclasses_excel/__init__.py
@@ -7,17 +7,24 @@ from openpyxl.cell import Cell
 log = logging.getLogger(__name__)
 
 # - find the cell/range, based on the field name, or perhaps some alias
+# - cell/range comment could also provide input
+# - openpyxl does some conversion based upon the formatting
 # - perform conversion based upon the defined name contents first
+# - convertion based on cell
 # - perform conversion based upon the field metadata, and then the default
 
 
 def _resolve_defined_name(field: Field, wb: Workbook, defn):
     log.debug(f"Resolving {field.name} from {defn.attr_text}")
     for dest in defn.destinations:
-        # this resolves to the cell or range
+        # this resolves to the cell or range - data_type is n/s/d
         cell: Cell = wb[dest[0]][dest[1]]
-        log.debug(f"\t= {cell.value!r}")
+        log.debug(f"\t= {cell.value!r}: {cell.data_type}")
         return cell.value
+
+    # if no matching field, but the type is structured then use that
+
+    # field.type is another dataclass then we can recurse on the dotted names
 
     raise ValueError(f"Could not find any destinations for {defn}")
 
