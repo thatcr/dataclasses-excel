22cec9988ba00e8bc859ecb1495320ecb8a750d5
Rob Thatcher <rob@tin.works>
2024-06-14 08:01:45 +0100

    wip: ndim function.

--- src/dataclasses_excel/__init__.py
+++ src/dataclasses_excel/__init__.py
@@ -7,6 +7,13 @@ from openpyxl.cell import Cell
 
 log = logging.getLogger(__name__)
 
+# problem is parallel recursive descent of the type spec and the excel structure
+
+# openpyxl: Cell, Tuple[Cell], List[Tuple[Cell]]
+# xlwings:
+# xlrd:
+
+
 # - find the cell/range, based on the field name, or perhaps some alias
 # - cell/range comment could also provide input
 # - openpyxl does some conversion based upon the formatting
@@ -15,7 +22,27 @@ log = logging.getLogger(__name__)
 # - perform conversion based upon the field metadata, and then the default
 
 
-def _resolve_defined_name(field: Field, wb: Workbook, defn):
+def _ndims(type: type):
+    if issubclass(get_origin(type) or type, (tuple, list)):
+        return 1 + sum(_ndims(arg) for arg in get_args(type))
+    return 0
+
+
+# each type has an origin and args
+# we only accept list/tuple with two levels of nesting and arg
+# arg can be any atomic type with a conversion, or datetime, or date.
+# do the base case type -> cell first.
+# the do the recursive bit - need to capture 1d lists first.
+
+# if we see a (Cell, ) tuple that can convert to an atomic.
+# atomic == not a list or tuple
+
+# how do dicts fit in - they have to parse the cell structure
+# dataclasses can also be parsed from just a range by matching the
+# cell names, or maybe aliases.
+
+
+def _resolve_defined_name(field: Field, type_: type, wb: Workbook, defn):
     log.debug(f"Resolving {field.name} from {defn.attr_text}")
 
     # usetyping.get_origin to figure out if it's a list
@@ -23,12 +50,7 @@ def _resolve_defined_name(field: Field, wb: Workbook, defn):
         # this resolves to the cell or range - data_type is n/s/d
         cell: Cell = wb[dest[0]][dest[1]]
 
-        # many way problem - cast from single or 2d matrix into the
-        # required shape
-
-        # the shape comes from the type: List[int] 1D List[Tuple[int]] 2D
-
-        if type(cell) is Cell:
+        if isinstance(cell, Cell):
             log.debug(f"\t= {cell.value!r}: {cell.data_type}")
             return cell.value
 
@@ -54,7 +76,9 @@ def read_excel(cls, wb: Workbook):
         raise TypeError(f"{cls!s} is not a dataclass")
 
     kwargs = {
-        field.name: _resolve_defined_name(field, wb, wb.defined_names[field.name])
+        field.name: _resolve_defined_name(
+            field, field.type, wb, wb.defined_names[field.name]
+        )
         for field in fields(cls)
     }
     return cls(**kwargs)
